731626c5b6f6c912ff1ccd1183dd1df355c7dc5a
Chiminii <aidoanchau@gmail.com>
2026-09-02 11:58:05 +0200

    loops and more

---
+++ multiplication-table.cs
@@ -0,0 +1,18 @@
+//int table = 5;
+//Nested loops (loops inside loops)
+//A small example with only two levels
+
+//The important to understand
+//The inner loop repeats until condition is false
+//Then the outer loop can continue...
+
+for(int i = 1; i <= 12; i++)
+{
+    Console.Write("");
+    Console.WriteLine($"{i}:ans tabell");
+    Console.WriteLine("----------------");
+    for(int j = 1; j <= 12; j++)
+    {    
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+}
